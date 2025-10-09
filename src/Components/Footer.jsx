0612ebdc6d88b7642e90a95bb1b5c1e78eb276be
Sultan Mia <sultanmia5732@gmail.com>
2025-10-09 21:13:01 +0600

    update footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,47 +6,57 @@ import xImg from '../assets/x.png.png'
 const Footer = () => {
     return (
         <footer className='bg-[#001931] px-20 py-10'>
-          <div className='flex flex-col md:flex-row justify-between items-center md:items-start gap-10 mb-10'>
+            <div className='flex flex-col md:flex-row justify-between items-center md:items-start gap-10 mb-10'>
 
-             <div className='flex items-center gap-1 justify-center mb-5 md:mb-0 text-center'>
-                   <a className=" text-xl"> <img src={logoImg} alt="" className='max-w-10 max-h-10' /> </a>
-                   <p className='text-white'>HERO.IO</p>
-            </div>
-
-            <div className='text-white'>
-                <h2 className='text-xl mb-3'> Company </h2>
-                <ul className='space-y-4'>
-                    <li> About us </li>
-                    <li> Contact </li>
-                    <li> Privacy Policy </li>
-                    <li> Service </li>
-                </ul>
-            </div>
-
-            <div>
-                <h2 className='text-white mb-3 text-xl'>Social Links</h2>
-                <div className='flex flex-col justify-between space-y-4 items-center gap-2'>
-
-                    <div className='flex items-center gap-2 flex-start'>
-                    <img src={xImg} alt="" />
-                    <a target='_blank' href="https://x.com/i/flow/single_sign_on" className='text-white'> Twitter </a>
-                    </div>
-
-                    <div className='flex items-center gap-2 flex-start'>
-                    <img src={inImg} alt="" />
-                    <a target='_blank' href="https://x.com/i/flow/single_sign_on" className='text-white'> LinkedIn </a>
-                    </div>
-
-                      <div className='flex items-center gap-2 flex-start'>
-                    <img src={fImg} alt="" />
-                    <a target='_blank' href="https://x.com/i/flow/single_sign_on" className='text-white'> Facebook </a>
-                    </div>
-                   
+                <div className='flex items-center gap-1 justify-center mb-5 md:mb-0 text-center'>
+                    <a className=" text-xl"> <img src={logoImg} alt="" className='max-w-10 max-h-10' /> </a>
+                    <p className='text-white'>HERO.IO</p>
                 </div>
-            </div>
 
-          </div>
-           <p className='text-white text-center'>Copyright © 2025 - All right reserved</p>
+                <div className="col text-white">
+                    <h4 className='text-xl mb-3'>Explore</h4>
+                    <ul className='space-y-4 text-white'>
+                        <li><a href="/categories">Categories</a></li>
+                        <li><a href="/top-apps">Top Apps</a></li>
+                        <li><a href="/new">New Releases</a></li>
+                        <li><a href="/reviews">Reviews</a></li>
+                    </ul>
+                </div>
+
+                <div className='text-white'>
+                    <h2 className='text-xl mb-3'> Company </h2>
+                    <ul className='space-y-4'>
+                        <li> About us </li>
+                        <li> Contact </li>
+                        <li> Privacy Policy </li>
+                        <li> Service </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 className='text-white mb-3 text-xl'>Social Links</h2>
+                    <div className='flex flex-col justify-between space-y-4 items-center gap-2'>
+
+                        <div className='flex items-center gap-2 flex-start'>
+                            <img src={xImg} alt="" />
+                            <a target='_blank' href="https://x.com/i/flow/single_sign_on" className='text-white'> Twitter </a>
+                        </div>
+
+                        <div className='flex items-center gap-2 flex-start'>
+                            <img src={inImg} alt="" />
+                            <a target='_blank' href="https://x.com/i/flow/single_sign_on" className='text-white'> LinkedIn </a>
+                        </div>
+
+                        <div className='flex items-center gap-2 flex-start'>
+                            <img src={fImg} alt="" />
+                            <a target='_blank' href="https://x.com/i/flow/single_sign_on" className='text-white'> Facebook </a>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <p className='text-white text-center'>Copyright © 2025 - All right reserved</p>
         </footer>
     );
 };
